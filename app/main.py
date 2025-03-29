1ce5bda7be2476cd6339ff84b242758570a6495d
Aidan Harrison <aidanharrison11@gmail.com>
2025-03-29 13:42:09 -0300

    Created a User table, allowed the front end to query the list of users in the database

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 from flask import request, jsonify
-from config import app
-from markupsafe import escape
+from models import User
+from config import app, db
 
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {name}!"
+@app.route("/createuser/<username>")
+def createUser(username):
+    new_user = User(userName=username)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        return e
+
+    return f"<p>User: {username} created</p>"
 
 
-@app.route("/<int:post_id>")
-def show_post(post_id):
-    return f"Post {post_id}"
+@app.route("/listusers")
+def listUsers():
+    users = User.query.all()
+    userString = ""
+    for user in users:
+        userString += user.__str__()
+    return f"List of users:{userString}"
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(ssl_context="adhoc", debug=True)
--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from config import db
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(80), unique=False, nullable=False)
+
+    def to_json(self):
+        return {"id": self.id, "userName": self.userName}
+
+    def __str__(self):
+        return f"ID:{self.id}, userName:{self.userName}\t"
