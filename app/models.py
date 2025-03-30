5a493a096919b9cf6f19d419bbe119be6a109f36
Aidan Harrison <aidanharrison11@gmail.com>
2025-03-30 12:31:57 -0300

    Added argon2 hashing and created the basic user type

--- app/models.py
+++ app/models.py
@@ -1,1 +1,19 @@
+from config import db
+from argon2 import PasswordHasher
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primaryKey=True)
+    userName = db.Column(db.String(80), unique=True, nullable=False)
+    passwordHash = db.Column(db.String(128), nullable=False)
+
+    def setPassword(self, password):
+        ph = PasswordHasher(time_cost=5, memory_cost=12288, parallelism=1)
+        self.passwordHash = ph.hash(password)
+
+    def checkPassword(self, password):
+        ph = PasswordHasher(time_cost=5, memory_cost=12288, parallelism=1)
+        return ph.verify(self.passwordHash, password)
+
+    def to_json(self):
+        return {"id": self.id, "userName": self.userName}
