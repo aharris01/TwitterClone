30b9116c15b113f6a7a42ffe255c805fc338e884
Aidan Harrison <aidanharrison11@gmail.com>
2025-03-30 10:04:28 -0300

    Tried to have it list users given their id

--- app/main.py
+++ app/main.py
@@ -24,6 +24,14 @@ def listUsers():
     return f"List of users:{userString}"
 
 
+@app.route("/listuser/<int:userID>")
+def listUser(userID):
+    user = db.session.execute(db.select(User).where(User.id == userID)).scalars()
+    print(user)
+
+    return f"<p>User: {user}</p>"
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
